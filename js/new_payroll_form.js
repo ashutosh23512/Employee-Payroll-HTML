39ac09d8d000f2ecf597db7a14b225a5a189b680
ASHUTOSH <ashutosh@192.168.1.6>
2020-11-18 23:35:08 +0530

    [Ashutosh]Added:Save in Local Storage

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -23,10 +23,22 @@ salary.addEventListener('input', function(){
 const save = () =>{
   try{
       let employeePayrollData = createEmployeePayroll();
+      createAndUpdateStorage(employeePayrollData);
   } catch(e){
       return;
   }
 
+}
+function createAndUpdateStorage(employeePayrollData){
+  let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+
+  if(employeePayrollList != undefined){
+      employeePayrollList.push(EmployeePayrollData);
+  } else{
+      employeePayrollList = [employeePayrollData];
+  }
+  alert(employeePayrollList.toString());
+  localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
 
 const createEmployeePayroll = () => {
